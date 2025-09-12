2d958df50df0ec1bae1c79cc4c203d599354ceca
Nahuel Frias <nahuelfrias@outlook.com.ar>
2025-09-12 09:58:02 -0300

    creo services para editar el perfil

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,11 @@ import { Stack } from "expo-router";
 
 const StackLayout = () => {
   return <Stack screenOptions={{ headerShown: false}}>
-    <Stack.Screen name="(modals)/profileModal" options={{ presentation: "modal"}} />
+    <Stack.Screen 
+    name="(modals)/profileModal" 
+    options={{ 
+      presentation: "modal"
+    }} />
   </Stack>
 }
 
